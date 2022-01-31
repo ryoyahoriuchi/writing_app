08bd9de4a23a28298293087d514f19c5282099f0
ryoya horiuchi <appleorange0720@gmail.com>
2022-01-31 15:00:51 +0900

    ツイッタークローンアプリ課題_提出1回目

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module WritingApp
+  class Application < Rails::Application
+    config.load_defaults 6.0
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = 'local'
+
+  end
+end
